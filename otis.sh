e545e950072e8b5c24577e44a8c0dd87cd60ee86
cehcuhl <csmith.github@gmail.com>
2019-04-30 18:26:18 -0400

    Update otis.sh

--- otis.sh
+++ otis.sh
@@ -1,17 +1,17 @@
-#!/usr/bin/env bash
+# Do some unimportant stuff
 
 loadkeys us
 locale-gen
 timedatectl set-ntp true
 
-# Get some info
+# Lets get some info
 echo "Choose a hostname:"
 read hostname
 
 echo "Enter user account name (lowercase, no spaces):"
 read username
 
-# Partition and mount hard drive
+# Partition and mount harddrive
 
 lsblk
 echo ""
@@ -21,48 +21,48 @@ echo "Drivetype (UEFI or BIOS):"
 read drivetype
 
 partition() {
-if [[ "$drivetype" == "UEFI" ]]; then
-	parted ${drive} mklabel gpt
-	parted ${drive} mkpart ESP fat32 1MiB 513MiB
-	parted ${drive} set 1 boot on
-	parted ${drive} mkpart primary ext4 513MiB 20GiB
-	parted ${drive} mkpart primary linux-swap 20GiB 24GiB
-	parted ${drive} mkpart primary ext4 24GiB 100%
+if [ "$drivetype" == "UEFI" ]; then
+	parted $drive mklabel gpt
+	parted $drive mkpart ESP fat32 1MiB 513MiB
+	parted $drive set 1 boot on
+	parted $drive mkpart primary ext4 513MiB 20GiB
+	parted $drive mkpart primary linux-swap 20GiB 24GiB
+	parted $drive mkpart primary ext4 24GiB 100%
 else
-	parted ${drive} mklabel msdos
-	parted ${drive} mkpart primary ext4 1MiB 20GiB
-	parted ${drive} set 1 boot on
-	parted ${drive} mkpart primary linux-swap 20GiB 24GiB
-	parted ${drive} mkpart primary ext4 24GiB 100%
+	parted $drive mklabel msdos
+	parted $drive mkpart primary ext4 1MiB 20GiB
+	parted $drive set 1 boot on
+	parted $drive mkpart primary linux-swap 20GiB 24GiB
+	parted $drive mkpart primary ext4 24GiB 100%
 fi
 }
 
 prepareDrive() {
-if [[ "$drivetype" == "UEFI" ]]; then
-	mkfs.vfat -F32 ${drive}\1
-	mkfs.ext4 -F ${drive}\2
-	mkfs.ext4 -F ${drive}\4
-	mkswap ${drive}\3
-	swapon ${drive}\3
-	mount ${drive}\2 /mnt
+if [ "$drivetype" == "UEFI" ]; then
+	mkfs.vfat -F32 $drive\1
+	mkfs.ext4 -F $drive\2
+	mkfs.ext4 -F $drive\4
+	mkswap $drive\3
+	swapon $drive\3
+	mount $drive\2 /mnt
 	mkdir /mnt/boot
 	mkdir /mnt/home
-	mount ${drive}\1 /mnt/boot
-	mount ${drive}\4 /mnt/home
+	mount $drive\1 /mnt/boot
+	mount $drive\4 /mnt/home
 else
-	mkfs.ext4 -F ${drive}\1
-	mkswap ${drive}\2
-	swapon ${drive}\2
-	mkfs.ext4 -F ${drive}\3
+	mkfs.ext4 -F $drive\1
+	mkswap $drive\2
+	swapon $drive\2
+	mkfs.ext4 -F $drive\3
 	mkdir /mnt
-	mount ${drive}\1 /mnt
+	mount $drive\1 /mnt
 	mkdir /mnt/home
-	mount ${drive}\3 /mnt/home
+	mount $drive\3 /mnt/home
 fi
 }
 
-partition ${drive}
-prepareDrive ${drive}
+partition $drive
+prepareDrive $drive
 
 # Install reflector and rate mirrors
 
@@ -84,7 +84,7 @@ arch-chroot /mnt hwclock --systohc --utc
 
 # Configure hostname
 
-echo ${hostname} > /mnt/etc/hostname
+echo $hostname > /mnt/etc/hostname
 echo "#<ip-address>	<hostname.domain.org>	<hostname>
 127.0.0.1		localhost.localdomain	localhost	$hostname
 ::1		localhost.localdomain	localhost	$hostname" > /mnt/etc/hosts
@@ -111,7 +111,7 @@ arch-chroot /mnt passwd
 
 # Install and configure bootloader
 
-if [[ "$drivetype" == "UEFI" ]]; then
+if [ "$drivetype" == "UEFI" ]; then
 	touch /mnt/boot/loader/entries/arch.conf
 	arch-chroot /mnt pacman -S dosfstools --noconfirm
 	arch-chroot /mnt bootctl --path=/boot install
@@ -123,15 +123,15 @@ if [[ "$drivetype" == "UEFI" ]]; then
 else
 	mkdir -p /mnt/boot/grub/
 	arch-chroot /mnt pacman -S grub os-prober --noconfirm
-	arch-chroot /mnt grub-install --recheck ${drive}
+	arch-chroot /mnt grub-install --recheck $drive
 	arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 fi
 
 # Create user account
 
-arch-chroot /mnt useradd -m -G wheel -s /bin/bash ${username}
-echo "Enter user account password for ${username}:"
-arch-chroot /mnt passwd ${username}
+arch-chroot /mnt useradd -m -G wheel -s /bin/bash $username
+echo "Enter user account password for $username:"
+arch-chroot /mnt passwd $username
 
 # End installation
 
